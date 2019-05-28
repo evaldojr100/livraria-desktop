e9e62e01dc9114b4039d0faf04c7f0e337ab5e17
Evaldo <evaldojr_melo@hotmail.com>
2019-05-28 19:07:40 -0400

    Criando View

---
+++ src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/autor_formulario.fxml"));
+        primaryStage.setTitle("Livraria Do Gabriel");
+        primaryStage.setScene(new Scene(root, 605, 228));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
